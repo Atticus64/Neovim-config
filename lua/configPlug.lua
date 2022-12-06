cf7817c245f16234642e3066f6de8b3d4f119a25
Jona <jonathanelian64@gmail.com>
2022-12-05 19:11:50 -0600

    Config updated

--- lua/configPlug.lua
+++ lua/configPlug.lua
@@ -1,5 +1,22 @@
 local config = vim.env.MYVIMRC
 -- You can change this variable to your dotfiles
+require('prettier').setup {
+	  bin = 'prettier', -- or `'prettierd'` (v0.22+)
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
+}
 
 require('lualine').setup {
     options = { theme  = 'onedark' },
@@ -10,10 +27,8 @@ require("nvim-tree").setup {
 require'bufferline'.setup {
     -- Enable/disable animations
     animation = true,
-  
     -- Enable/disable auto-hiding the tab bar when there is a single buffer
     auto_hide = false,
-  
     -- Enable/disable current/total tabpages indicator (top right corner)
     tabpages = true,
   
@@ -37,7 +52,7 @@ require'bufferline'.setup {
   local db = require('dashboard')
   -- only for linux
   -- install lolcat to use
-  -- db.preview_command = 'cat | lolcat'
+  db.preview_command = 'cat | lolcat'
 
   db.custom_header = {
 		[[███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗]],
@@ -84,24 +99,10 @@ require'nvim-treesitter.configs'.setup {
   -- Automatically install missing parsers when entering buffer
   auto_install = true,
 
-  -- List of parsers to ignore installing (for "all")
-
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
 }
@@ -111,9 +112,6 @@ require('nvim_comment').setup()
 
 
 require('telescope').setup {
-		-- file_ignore_patterns = {
-		-- 		"**/node_modules/**"
-		-- }
 		defaults = { 
       file_ignore_patterns = {
         "node_modules", 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,57 +1,17 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
--- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
     use 'wbthomason/packer.nvim'
-
-    --   -- Lazy loading:
-    --   -- Load on specific commands
-    --   use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}
-
-    --   -- Load on an autocommand event
-    --   use {'andymass/vim-matchup', event = 'VimEnter'}
-
-    --   -- Load on a combination of conditions: specific filetypes or commands
-    --   -- Also run code after load (see the "config" key)
-    --   use {
-    --     'w0rp/ale',
-    --     ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'racket', 'vim', 'tex'},
-    --     cmd = 'ALEEnable',
-    --     config = 'vim.cmd[[ALEEnable]]'
-    --   }
-
-    --   -- Plugins can have dependencies on other plugins
-    --   use {
-    --     'haorenW1025/completion-nvim',
-    --     opt = true,
-    --     requires = {{'hrsh7th/vim-vsnip', opt = true}, {'hrsh7th/vim-vsnip-integ', opt = true}}
-    --   }
-
-    --   -- Plugins can also depend on rocks from luarocks.org:
-    --   use {
-    --     'my/supercoolplugin',
-    --     rocks = {'lpeg', {'lua-cjson', version = '2.1.0'}}
-    --   }
-
-    --   -- You can specify rocks in isolation
-    --   use_rocks 'penlight'
-    --   use_rocks {'lua-resty-http', 'lpeg'}
-
-    --   -- Local plugins can be included
-    --   use '~/projects/personal/hover.nvim'
-
-    --   -- Plugins can have post-install/update hooks
-    --   use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
-
-    -- Post-install/update hook with neovim command
-    use {
+   use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
 
-    -- Post-install/update hook with call of vimscript function with argument
+		-- Prettier
+		use {
+			'MunifTanjim/prettier.nvim'
+		}
+
     use {
         'glacambre/firenvim',
         run = function()
@@ -65,12 +25,6 @@ return require('packer').startup(function(use)
 
   -- Dashboard
 	  use {'glepnir/dashboard-nvim'}
-
-    -- Use specific branch, dependency and run lua file after load
-    -- use {
-    --   'glepnir/galaxyline.nvim', branch = 'main', config = function() require'statusline' end,
-    --   requires = {'kyazdani42/nvim-web-devicons'}
-    -- }
 
     -- Complements
 
@@ -97,7 +51,6 @@ return require('packer').startup(function(use)
   		'phaazon/hop.nvim',
   		branch = 'v2', -- optional but strongly recommended
   		config = function()
-    	-- you can configure Hop the way you like here; see :h hop-config
     	require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
   		end
 		}
@@ -124,19 +77,13 @@ return require('packer').startup(function(use)
         "aserowy/tmux.nvim",
         config = function()
             require("tmux").setup({
-                -- overwrite default configuration
-                -- here, e.g. to enable default bindings
-                copy_sync = {
-                    -- enables copy sync and overwrites all register actions to
-                    -- sync registers *, +, unnamed, and 0 till 9 from tmux in advance
-                    enable = true
-                },
+								copy_sync = { 
+									enable = true
+  							},
                 navigation = {
-                    -- enables default keybindings (C-hjkl) for normal mode
                     enable_default_keybindings = true
                 },
                 resize = {
-                    -- enables default keybindings (A-hjkl) for normal mode
                     enable_default_keybindings = true
                 }
             })
@@ -179,15 +126,6 @@ return require('packer').startup(function(use)
       'romgrk/barbar.nvim',
       requires = {'kyazdani42/nvim-web-devicons'}
     }
-
-    -- Use dependency and run lua function after load
-    -- use {
-    --     'lewis6991/gitsigns.nvim',
-    --     requires = {'nvim-lua/plenary.nvim'},
-    --     config = function()
-    --         require('gitsigns').setup()
-    --     end
-    -- }
 
     -- You can alias plugin names
     use {
